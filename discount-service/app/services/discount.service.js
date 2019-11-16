73190e564734d7d7c3c758a04cb7aa203c61cddd
Kaio Ferreira <kaiolf11@gmail.com>
2019-11-16 15:19:51 -0300

    discount: added service layer implementation

---
+++ discount-service/app/services/discount.service.js
@@ -0,0 +1,57 @@
+const User = require('../models/User')
+const Product = require('../models/Product')
+
+var BLACK_FRIDAY = {
+  day: 25,
+  month: 11,
+}
+
+function compareDayAndMonth(d1, d2) {
+  return d1.getDate() == d2.getDate() && d2.getMonth() == d3.getMonth()
+}
+
+function isBlackFriday() {
+  const today = new Date()
+  const blackFriday = new Date(`${today.getFullYear()}-${BLACK_FRIDAY.month}-${BLACK_FRIDAY.day}`)
+  return compareDayAndMonth(today, blackFriday)
+}
+
+function isUserBirthday(birthday) {
+  const today = new Date()
+  return compareDayAndMonth(today, birthday)
+}
+
+async function getProductDiscount({ productID, userID }) {
+  let pct = 0
+
+  const dob = await User.findByPk(userID)
+    .then(user => new Date(user.get('dateOfBirth')))
+    .catch(err => Promise.reject(err))
+
+  if (isUserBirthday(dob))
+    pct = .05 
+
+  if (isBlackFriday())
+    pct = .10
+
+  const priceInCents = await Product.findByPk(productID)
+    .then(p => p.priceInCents)
+    .catch(err => Promise.reject(err))
+
+  return { 
+    discount: {
+      priceInCents: priceInCents * pct, 
+      percentual: pct * 100
+    }
+  }
+}
+
+module.exports = {
+  GetDiscount: async (call, callback) => {
+    try {
+      callback(null, await getProductDiscount(call.request))
+    } catch(err) {
+      callback(null, err)
+    }
+  }
+}
